8f124368093e5c86b04bf13c8ca2b15e69618cc1
Jerusalem Lipardo <beigecolor29@gmail.com>
2019-08-01 19:07:22 -0700

    static page completed

--- portfolio-app/src/index/Index.js
+++ portfolio-app/src/index/Index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./Header/Header";
+import Contact from "./Contact/Contact";
 
 const index = () => {
   return (
     <div>
       <Header />
+      <Contact />
     </div>
   );
 };
