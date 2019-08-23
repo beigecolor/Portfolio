903d6b4309c11304ea8eb792d6d3b83cbc46c157
Jerusalem Lipardo <beigecolor29@gmail.com>
2019-08-22 20:51:38 -0700

    backend v1.1 making containers and components

--- portfolio-backend/src/App.js
+++ portfolio-backend/src/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from "react";
+import { Switch, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Signup from "./container/Signup";
 
 class App extends Component {
   render() {
     return (
       <div className="container">
-        <h1>Hello World</h1>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/signup" conponent={Signup} />
+        </Switch>
       </div>
     );
   }
